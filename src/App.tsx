7c46309181ea0c9a0fe06e4046d0b7d10b4505a2
manipapan2 <manipapan2@gmail.com>
2026-08-28 15:09:53 +0330

    feat: add detail section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,80 @@
-import Header from './components/header'
-import './global.css'
+import { createContext, useState } from "react";
+import DetailSection from "./components/sections/detail";
+import "./global.css";
+import Header from "./components/header";
+export interface ApartmantProps {
+  year?: string | null;
+  month?: string | null;
+  fee?: number | null;
+  floorCount?: number | null;
+  unitCount?: number | null;
+  neighbors?: NeighborProps;
+  costs?: CostProps;
+  moneyLeft?: MoneyLeftProps;
+}
+
+interface NeighborProps {
+  [key: number | string]: {
+    name: string | null;
+    floor: number | null;
+    unit: number | null;
+    hasPaidFee: boolean;
+  };
+}
+
+export interface CostProps {
+  [key: number | string]: {
+    title: string | null;
+    cost: number | null;
+  };
+}
+
+export type MoneyLeftProps = CostProps;
+
+interface ApartmantContextProps {
+  apartmantData: ApartmantProps;
+  setApartmantData: React.Dispatch<React.SetStateAction<ApartmantProps>>;
+}
+
+const ApartmantContextInitialState: ApartmantContextProps = {
+  apartmantData: {
+    costs: {
+      0: {
+        title: "",
+        cost: null,
+      },
+    },
+  },
+  setApartmantData: () => null,
+};
+
+export const ApartmantContext = createContext<ApartmantContextProps>(
+  ApartmantContextInitialState,
+);
 
 function App() {
+  const [apartmantData, setApartmantData] = useState<ApartmantProps>({
+    costs: {
+      0: {
+        title: "",
+        cost: null,
+      },
+    },
+  });
+
 
   return (
-    <Header/>
-  )
+    <ApartmantContext value={{ apartmantData, setApartmantData }}>
+      <div className="flex flex-col w-full h-full max-h-full">
+        <Header />
+        <main className="flex grow overflow-y-auto">
+          <div className="w-fit flex flex-col p-4 max-h-full h-full flex-1 overflow-y-auto">
+            <DetailSection />
+          </div>
+        </main>
+      </div>
+    </ApartmantContext>
+  );
 }
 
 export default App
---
+++ src/components/sections/detail.tsx
@@ -0,0 +1,165 @@
+import { useContext, useEffect } from "react";
+import { Input } from "../ui/input";
+import Title from "../title";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { ApartmantContext, type ApartmantProps } from "@/App";
+
+const persianMonthItems = [
+  { label: "فروردین", value: "فروردین" },
+  { label: "اردیبهشت", value: "اردیبهشت" },
+  { label: "خرداد", value: "خرداد" },
+  { label: "تیر", value: "تیر" },
+  { label: "مرداد", value: "مرداد" },
+  { label: "شهریور", value: "شهریور" },
+  { label: "مهر", value: "مهر" },
+  { label: "آیان", value: "آیان" },
+  { label: "آذز", value: "آذز" },
+  { label: "دی", value: "دی" },
+  { label: "بهمن", value: "بهمن" },
+  { label: "اسفند", value: "اسفند" },
+];
+const currentPersianMonth = new Date().toLocaleDateString("fa-IR", {
+  month: "long",
+});
+
+const convertPersianNumberToEnglish = (s: string) =>
+  s.replace(/[۰-۹]/g, (d) => "۰۱۲۳۴۵۶۷۸۹".indexOf(d).toString());
+const currentPersianYear = convertPersianNumberToEnglish(
+  new Date().toLocaleDateString("fa-IR", { year: "numeric" }),
+);
+let persianYearItems: {
+  label: string;
+  value: null | string;
+  // change
+}[] = Array.from({ length: 6 }).map((_, index) => {
+  const calculatedYear = (parseInt(currentPersianYear) - index).toString();
+  return { label: calculatedYear, value: calculatedYear };
+});
+// persianYearItems.unshift({ label: "انتخاب ماه", value: null });
+
+export default function DetailSection() {
+  const apartmantContext = useContext(ApartmantContext);
+  const { setApartmantData } = apartmantContext;
+
+  useEffect(() => {
+    setApartmantData((prevState: ApartmantProps) => {
+      const clonedObj = { ...prevState };
+      clonedObj["year"] = persianYearItems[0].value;
+      clonedObj["month"] = currentPersianMonth;
+      return clonedObj;
+    });
+  }, []);
+
+  return (
+    <section className="rounded-md bg-secondary p-3 flex flex-col gap-5">
+      <div>
+        <Title>مبلغ شارژ</Title>
+        <Input
+          unit="تومان"
+          separateNumbers
+          onValue={(value) =>
+            setApartmantData((prevState: ApartmantProps) => {
+              const clonedObj = { ...prevState };
+              clonedObj["fee"] = parseInt(value);
+              return clonedObj;
+            })
+          }
+          placeholder="مثلا: 400,000"
+        />
+        {/* <input type="text" onChange={() => console.log('ttttt')} value={''} /> */}
+      </div>
+      <div>
+        <Title>تاریخ</Title>
+        <div className="flex gap-2">
+          <Select
+            items={persianMonthItems}
+            defaultValue={currentPersianMonth}
+            onValueChange={(value) =>
+              setApartmantData((prevValue: ApartmantProps) => {
+                const clonedArray = { ...prevValue };
+                clonedArray["month"] = value;
+                return clonedArray;
+              })
+            }
+          >
+            <SelectTrigger className="w-full max-w-48">
+              <SelectValue />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>ماه ها</SelectLabel>
+                {persianMonthItems.map((item) => (
+                  <SelectItem key={item.value} value={item.value}>
+                    {item.label}
+                  </SelectItem>
+                ))}
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+          <Select
+            items={persianYearItems}
+            defaultValue={persianYearItems[0].value}
+            onValueChange={(value) =>
+              setApartmantData((prevValue: ApartmantProps) => {
+                const clonedObject = { ...prevValue };
+                clonedObject["year"] = value;
+                return clonedObject;
+              })
+            }
+          >
+            <SelectTrigger className="w-full max-w-48">
+              <SelectValue />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>سال ها</SelectLabel>
+                {persianYearItems.map((item) => (
+                  <SelectItem key={item.value} value={item.value}>
+                    {item.label}
+                  </SelectItem>
+                ))}
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
+      </div>
+      <div>
+        <Title>طبقات</Title>
+        <div className="flex gap-2">
+          <Input
+            type="number"
+            placeholder="تعداد طبقه ها"
+            maxLength={1}
+            onValue={(value) =>
+              setApartmantData((prevState: ApartmantProps) => {
+                const clonedObj = { ...prevState };
+                clonedObj["floorCount"] = parseInt(value);
+                return clonedObj;
+              })
+            }
+          />
+          <Input
+            type="number"
+            placeholder="تعداد واحد ها"
+            maxLength={1}
+            onValue={(value) =>
+              setApartmantData((prevState: ApartmantProps) => {
+                const clonedObj = { ...prevState };
+                clonedObj["unitCount"] = parseInt(value);
+                return clonedObj;
+              })
+            }
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
