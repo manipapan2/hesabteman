826c8ae3179425b3f5be6a8901e0301560a39052
manipapan2 <manipapan2@gmail.com>
2026-08-28 15:13:13 +0330

    feat: create function for floor number to persian word conversion

---
+++ src/utils/floorToPersianWord.ts
@@ -0,0 +1,21 @@
+export default function floorToPersianWord(number: string | number) {
+  switch (number) {
+    case 1:
+      return "اول";
+
+    case 2:
+      return "دوم";
+
+    case 3:
+      return "سوم";
+
+    case 4:
+      return "چهارم";
+
+    case 5:
+      return "پنجم";
+
+    default:
+      break;
+  }
+}
