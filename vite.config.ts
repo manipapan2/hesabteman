cd08234ba166fff61bca9ac2435196a47ad25ca3
manipapan2 <manipapan2@gmail.com>
2026-08-29 17:04:24 +0330

    refactor: configure PWA with vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,43 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      injectRegister: 'script',
+      manifest: {
+        name: "حسابتمان",
+        short_name: "حسابتمان",
+        start_url: "/",
+        description: "An app for creating an apartmant balance sheet",
+        theme_color: "#262626",
+        background_color: "#262626",
+        display: "standalone",
+        icons: [
+          {
+            src: "/web-app-manifest-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/web-app-manifest-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
