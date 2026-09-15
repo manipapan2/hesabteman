49db0301708a080b05794489f7b44d9207c1fe80
manipapan2 <manipapan2@gmail.com>
2026-09-15 20:16:20 +0330

    build: config vite-bundle-analyzer

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,14 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
+import { analyzer } from "vite-bundle-analyzer";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    analyzer(),
     VitePWA({
       registerType: "autoUpdate",
       injectRegister: "script",
