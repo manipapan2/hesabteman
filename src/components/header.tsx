d021c4dee019c03a1bc2b554207eb1362e099fe7
manipapan2 <manipapan2@gmail.com>
2026-09-15 20:40:09 +0330

    refactor: change app logo file extention from png to webp

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,7 @@
 import { Moon, Sun } from "lucide";
 import { useEffect, useLayoutEffect, useRef, useState } from "react";
 import { MorphIcon } from "morphicons/react";
-import AppLogo from "@/assets/images/app-logo.png";
+import AppLogo from "@/assets/images/app-logo.webp";
 
 export default function Header() {
   const [theme, setTheme] = useState<"dark" | "light">(() => {
