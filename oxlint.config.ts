0b8a422ecb70bf662f05cde1c67ecdc3446b354f
manipapan2 <manipapan2@gmail.com>
2026-09-15 15:17:45 +0330

    build: set some rules off

--- oxlint.config.ts
+++ oxlint.config.ts
@@ -1,3 +1,10 @@
 import { defineConfig } from "@fullstacksjs/oxlint-config";
 
-export default defineConfig();
+export default defineConfig({
+  rules: {
+    "import/no-cycle": "off",
+    "array-callback-return": "off",
+    "react/exhaustive-deps": "off",
+    "react/no-array-index-key": "off",
+  },
+});
