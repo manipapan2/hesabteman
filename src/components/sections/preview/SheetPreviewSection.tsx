407fe24e976cbe426b913ee16ed203e45132f200
manipapan2 <manipapan2@gmail.com>
2026-09-16 13:15:28 +0330

    refactor(preview): remove dark and light thmes

--- src/components/sections/preview/SheetPreviewSection.tsx
+++ src/components/sections/preview/SheetPreviewSection.tsx
@@ -7,10 +7,6 @@ import IconToggle from "../../IconToggle.tsx";
 
 export default function SheetPreviewSection() {
   const [isShown, setIsShown] = useState(false);
-  const darkSpanId = useId();
-  const lighSpanId = useId();
-  const darkSpan = document.getElementById(darkSpanId);
-  const lightSpan = document.getElementById(lighSpanId);
 
   useEffect(() => {
     if (isShown) {
@@ -37,49 +33,17 @@ export default function SheetPreviewSection() {
       >
         <div className="relative flex h-full w-fit flex-col items-center justify-center rounded-md">
           {/* <img alt="test" src={imageUrl} className="w-full rounded-md" /> */}
-          <span id={darkSpanId} className="dark hidden" />
-          <span id={lighSpanId} className="light hidden" />
           <div className="mb-2 w-full max-w-full">
-            {darkSpan && lightSpan && (
-              <SheetThemeSelector
-                colors={[
-                  {
-                    background: window
-                      .getComputedStyle(darkSpan)
-                      .getPropertyValue("--background"),
-                    backgroundForeground: window
-                      .getComputedStyle(darkSpan)
-                      .getPropertyValue("--background-foreground"),
-                    secondary: window
-                      .getComputedStyle(darkSpan)
-                      .getPropertyValue("--secondary"),
-                    secondaryForeground: window
-                      .getComputedStyle(darkSpan)
-                      .getPropertyValue("--secondary-foreground"),
-                  },
-                  {
-                    background: window
-                      .getComputedStyle(lightSpan)
-                      .getPropertyValue("--background"),
-                    backgroundForeground: window
-                      .getComputedStyle(lightSpan)
-                      .getPropertyValue("--background-foreground"),
-                    secondary: window
-                      .getComputedStyle(lightSpan)
-                      .getPropertyValue("--secondary"),
-                    secondaryForeground: window
-                      .getComputedStyle(lightSpan)
-                      .getPropertyValue("--secondary-foreground"),
-                  },
-                  {
-                    background: "#1C283B",
-                    backgroundForeground: "white",
-                    secondary: "#2347a9",
-                    secondaryForeground: "white",
-                  },
-                ]}
-              />
-            )}
+            <SheetThemeSelector
+              colors={[
+                {
+                  background: "#1C283B",
+                  backgroundForeground: "white",
+                  secondary: "#2347a9",
+                  secondaryForeground: "white",
+                },
+              ]}
+            />
           </div>
           <div className="max-h-4/6 w-full">
             <Sheet />
