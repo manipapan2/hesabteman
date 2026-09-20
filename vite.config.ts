f0811614f164dc60521a08a4390a1bdd33a46317
manipapan2 <manipapan2@gmail.com>
2026-09-20 16:45:09 +0330

    build: config react compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
+import babel from "@rolldown/plugin-babel";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    babel({
+      presets: [reactCompilerPreset()],
+    }),
     tailwindcss(),
     VitePWA({
       registerType: "autoUpdate",
